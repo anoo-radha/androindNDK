dbc46d9e94f0dff34765e30f70df0fedda73302b
Anurad80 <anu_rad80@yahoo.com>
2016-11-11 09:48:55 -0500

    Added String operations

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,7 +5,13 @@ extern "C"
 jstring
 Java_com_anuradha_ndkapp_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject /* this */,
+        jstring inJNIStr) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+
+    const char *inCStr = (env)->GetStringUTFChars(inJNIStr,NULL);
+    if (NULL == inCStr) return NULL;
+
+//    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(inCStr);
 }
