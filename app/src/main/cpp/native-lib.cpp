1311d3d1755047d54512ef71859002420172b030
Anurad80 <anu_rad80@yahoo.com>
2016-11-16 15:37:41 -0500

    Add integer input and logging

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,17 +1,28 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C"
 jstring
 Java_com_anuradha_ndkapp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */,
-        jstring inJNIStr) {
+        jstring inJNIStr,
+        jint input_num) {
     std::string hello = "Hello from C++";
 
     const char *inCStr = (env)->GetStringUTFChars(inJNIStr,NULL);
     if (NULL == inCStr) return NULL;
 
+    //getting part of the input string
+    char outCStr[128];
+    (env)->GetStringUTFRegion(inJNIStr, 2,3,outCStr);
+
+    //halfing the input number by right shifting
+    input_num >>= 1;
+    //To redirect print message to Log.i
+    __android_log_print(ANDROID_LOG_INFO, "stringFromJNI", "halfed number is: %d", input_num);
+
 //    return env->NewStringUTF(hello.c_str());
-    return env->NewStringUTF(inCStr);
+    return env->NewStringUTF(outCStr);
 }
--- app/src/main/java/com/anuradha/ndkapp/MainActivity.java
+++ app/src/main/java/com/anuradha/ndkapp/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI("Did it!"));
+        tv.setText(stringFromJNI("Did it!", 17));
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI( String msg );
+    public native String stringFromJNI( String msg, int num );
 }
